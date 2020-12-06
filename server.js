7f7fe76d142d0eea4f50b9b81a4d740d74a08211
monkmitrad <monkmitrad@gmail.com>
2020-12-06 14:01:43 +0100

    added initial pages and routing

--- server.js
+++ server.js
@@ -4,12 +4,15 @@ const express = require('express');
 
 // Constants
 const PORT = 8080;
-const HOST = '0.0.0.0';
+const HOST = 'localhost';
 
 // App
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World');
+
+app.use('/', express.static(__dirname + '/views'));
+
+app.get('*/*', (req, res) => {
+  res.send('Hello World!');
 });
 
 app.listen(PORT, HOST);
