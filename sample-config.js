df4442cb6c7516ff17d11ab54d902a2ef241b847
monkmitrad <monkmitrad@gmail.com>
2021-01-31 15:52:29 +0100

    update packages and changed config

--- sample-config.js
+++ sample-config.js
@@ -3,9 +3,10 @@ var config = new Map();
 config.set('port', 8080);
 config.set('host', '0.0.0.0');
 config.set('app_folder', './public');
-config.set('db_host', 'db');
+config.set('db_host', 'mongo');
 config.set('db_port', '27017');
 config.set('db_name', 'whoami');
 config.set('jwt_secret', 'mySecret');
+config.set('api_baseURL', '/api/');
 
 module.exports = config;
